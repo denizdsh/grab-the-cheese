4a05c2479ec5100c54a010c8bd1691f5d130db2b
Deniz Shyukryuev <denizztto@gmail.com>
2023-01-13 16:17:57 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace grab_the_cheese
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
